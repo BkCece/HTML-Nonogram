71341ca40756fb815de37f6f2652be74b062b3fe
BkCece <BkCece@users.noreply.github.com>
2020-06-07 19:17:50 -0700

    reset button added

--- public/nono.js
+++ public/nono.js
@@ -1,3 +1,8 @@
+//Values for the answer key and user's table are all hardcoded
+//1: the tile should be shown black
+//2: the tile should be shown white
+//Size is 15x15 and has 4 additional rows & columns for the hint values
+
 //Nonogram design from http://nonogramskatana.blogspot.com/2016/05/15x15-mouse.html
 var tableAnswer = [
     [0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0],
@@ -45,23 +50,29 @@ var tableUser = [
     [0, 3, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
+//Creates the answer table as html code - a string
 function createTable(tbl) {
     var newTable = "<table>";
     for (var i = 0; i < tbl.length; i++) {
         newTable += "<tr>";
         for (var j = 0; j < tbl[i].length; j++) {
+            //check if guide row
+            //these can't be altered by the user & display differently
             if ((i < 4) || (j < 4)) {
                 if (tbl[i][j] == 0) {
+                    //don't display a number if value is zero
                     newTable += "<td id = guide> &nbsp; &nbsp; &nbsp; </td>";
                 } else {
+                    //display when the number is used as a guide for the puzzle
                     newTable += "<td id = guide> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
                 }
 
             } else {
+                //different id's to set cell colours if toggles/not
                 if (tbl[i][j] == 1) {
-                    newTable += "<td id = one> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
+                    newTable += "<td id = one> &nbsp; &nbsp; &nbsp; </td>";
                 } else {
-                    newTable += "<td id = zero> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
+                    newTable += "<td id = zero> &nbsp; &nbsp; &nbsp; </td>";
                 }
             }
 
@@ -73,6 +84,7 @@ function createTable(tbl) {
     return newTable;
 }
 
+//Creates the user table as html code - a string
 function createUserTable(tbl) {
     var newTable = "<table>";
     for (var i = 0; i < tbl.length; i++) {
@@ -87,9 +99,9 @@ function createUserTable(tbl) {
 
             } else {
                 if (tbl[i][j] == 1) {
-                    newTable += "<td id = one onclick='toggleCell(" + i + ", " + j + "); return false;'> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
+                    newTable += "<td id = one onclick='toggleCell(" + i + ", " + j + "); return false;'> &nbsp; &nbsp; &nbsp; </td>";
                 } else {
-                    newTable += "<td id = zero onclick='toggleCell(" + i + ", " + j + "); return false;'> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
+                    newTable += "<td id = zero onclick='toggleCell(" + i + ", " + j + "); return false;'> &nbsp; &nbsp; &nbsp; </td>";
                 }
             }
 
@@ -111,30 +123,16 @@ function toggleCell(r, c) {
     document.getElementById("userTable").innerHTML = createUserTable(tableUser);
 }
 
-function clearTable(tbl) {
-    var newTable = "<table>";
-    for (var i = 0; i < tbl.length; i++) {
-        newTable += "<tr>";
-        for (var j = 0; j < tbl[i].length; j++) {
-            if ((i < 4) || (j < 4)) {
-                if (tbl[i][j] == 0) {
-                    newTable += "<td id = guide> &nbsp; &nbsp; &nbsp; </td>";
-                } else {
-                    newTable += "<td id = guide> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
-                }
-
-            } else {
-                tbl[i][j] = 0;
-                //newTable += "<td id = zero onclick = \"toggleCell(" + i + ", " + j + ");\"> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
-                newTable += "<td id = zero> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
+function clearUserTable() {
+    for (var i = 0; i < tableUser.length; i++) {
+        for (var j = 0; j < tableUser[i].length; j++) {
+            if ((i > 3) && (j > 3)) {
+                tableUser[i][j] = 0;
             }
-
         }
-        newTable += "</tr>";
     }
-    newTable += "</table>";
 
-    return newTable;
+    document.getElementById("userTable").innerHTML = createUserTable(tableUser);
 }
 
 function toggleAnswerTable() {
@@ -147,8 +145,7 @@ function toggleAnswerTable() {
     }
 }
 
-var htmltableAnswer = createTable(tableAnswer);
 document.getElementById("text").innerHTML = "Here we go!";
 document.getElementById("userTable").innerHTML = createUserTable(tableUser);
-document.getElementById("answerTable").innerHTML = htmltableAnswer;
+document.getElementById("answerTable").innerHTML = createTable(tableAnswer);
 document.getElementById("answerTable").style.display = "none";
