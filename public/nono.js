09191c4aee0a067d3c594d8fdbf63e62e26922b5
BkCece <BkCece@users.noreply.github.com>
2020-06-07 22:39:10 -0700

    New puzzles options, css updates

--- public/nono.js
+++ public/nono.js
@@ -3,7 +3,10 @@
 //2: the tile should be shown white
 //Size is 15x15 and has 4 additional rows & columns for the hint values
 
+var hardPuzzleChosen = true;
+
 //Nonogram design from http://nonogramskatana.blogspot.com/2016/05/15x15-mouse.html
+//Default puzzle is the harder puzzle
 var tableAnswer = [
     [0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 1, 3, 2, 0, 4, 2, 1, 0],
@@ -24,7 +27,54 @@ var tableAnswer = [
     [0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
     [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
     [0, 0, 5, 2, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0],
-    [0, 3, 2, 3, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0],
+    [0, 3, 2, 3, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0]
+];
+
+var tableAnswerHard = [
+    [0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 1, 3, 2, 0, 4, 2, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 2, 1, 6, 2, 3, 2, 1, 1, 2, 2, 2, 1],
+    [0, 0, 0, 0, 2, 3, 1, 1, 1, 2, 2, 2, 5, 1, 1, 1, 2, 5, 5],
+
+    [0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [0, 4, 2, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+    [3, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+    [3, 1, 2, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0],
+    [0, 2, 1, 2, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0],
+    [0, 4, 2, 2, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
+    [0, 2, 2, 2, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+    [0, 1, 3, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [0, 2, 2, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1],
+    [0, 0, 4, 3, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 1, 2, 2, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 0, 5, 2, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+    [0, 3, 2, 3, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0]
+];
+
+var tableAnswerEasy = [
+    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 4, 3, 0, 0, 4, 1, 4, 0, 0, 3, 4, 0, 0],
+    [0, 0, 0, 0, 0, 7, 4, 7, 5, 4, 1, 1, 1, 4, 5, 7, 4, 7, 0],
+    [0, 0, 0, 0, 15, 7, 2, 3, 5, 4, 4, 1, 4, 4, 5, 3, 2, 7, 15],
+
+    [0, 0, 0, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 7, 7, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [2, 4, 4, 2, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1],
+    [0, 3, 7, 3, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+    [0, 0, 5, 5, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [0, 0, 4, 4, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [4, 1, 1, 4, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [0, 0, 4, 4, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 4, 4, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 5, 5, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [0, 3, 7, 3, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+    [2, 4, 2, 2, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1],
+    [0, 0, 7, 7, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
 ];
 
 var tableUser = [
@@ -47,7 +97,53 @@ var tableUser = [
     [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 3, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var tableUserHard = [
+    [0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 1, 3, 2, 0, 4, 2, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 2, 1, 6, 2, 3, 2, 1, 1, 2, 2, 2, 1],
+    [0, 0, 0, 0, 2, 3, 1, 1, 1, 2, 2, 2, 5, 1, 1, 1, 2, 5, 5],
+
+    [0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 4, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 4, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 5, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var tableUserEasy = [
+    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 4, 3, 0, 0, 4, 1, 4, 0, 0, 3, 4, 0, 0],
+    [0, 0, 0, 0, 0, 7, 4, 7, 5, 4, 1, 1, 1, 4, 5, 7, 4, 7, 0],
+    [0, 0, 0, 0, 15, 7, 2, 3, 5, 4, 4, 1, 4, 4, 5, 3, 2, 7, 15],
+
+    [0, 0, 0, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 4, 4, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 3, 7, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [4, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 3, 7, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 4, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 15, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
 //Creates the answer table as html code - a string
@@ -68,7 +164,7 @@ function createTable(tbl) {
                 }
 
             } else {
-                //different id's to set cell colours if toggles/not
+                //different id's to set cell colours if toggled/not
                 if (tbl[i][j] == 1) {
                     newTable += "<td id=one> &nbsp; &nbsp; &nbsp; </td>";
                 } else {
@@ -90,14 +186,19 @@ function createUserTable(tbl) {
     for (var i = 0; i < tbl.length; i++) {
         newTable += "<tr>";
         for (var j = 0; j < tbl[i].length; j++) {
+            //check if guide row
+            //these can't be altered by the user & display differently
             if ((i < 4) || (j < 4)) {
                 if (tbl[i][j] == 0) {
+                    //don't display a number if value is zero
                     newTable += "<td id=guide> &nbsp; &nbsp; &nbsp; </td>";
                 } else {
+                    //display when the number is used as a guide for the puzzle
                     newTable += "<td id=guide> &nbsp;" + tbl[i][j] + "&nbsp; </td>";
                 }
 
             } else {
+                //different id's to set cell colours if toggled/not
                 if (tbl[i][j] == 1) {
                     newTable += "<td id=one onclick='toggleCell(" + i + ", " + j + "); return false;'> &nbsp; &nbsp; &nbsp; </td>";
                 } else {
@@ -113,6 +214,8 @@ function createUserTable(tbl) {
     return newTable;
 }
 
+//Sets cell value to zero if 1, and to 1 of zero
+//Toggles cell value and re-displays table with new cell values
 function toggleCell(r, c) {
     if (tableUser[r][c] == 0) {
         tableUser[r][c] = 1;
@@ -121,13 +224,23 @@ function toggleCell(r, c) {
     }
 
     document.getElementById("userTable").innerHTML = createUserTable(tableUser);
+
+    //Check if correct
+    submitAnswer();
 }
 
+//Sets all user-accessable cells to 0
+//Does not change guide cells
 function clearUserTable() {
     for (var i = 0; i < tableUser.length; i++) {
         for (var j = 0; j < tableUser[i].length; j++) {
             if ((i > 3) && (j > 3)) {
-                tableUser[i][j] = 0;
+                if (hardPuzzleChosen) {
+                    tableUser[i][j] = 0;
+                } else {
+                    tableUser[i][j] = 1;
+                }
+
             }
         }
     }
@@ -135,6 +248,7 @@ function clearUserTable() {
     document.getElementById("userTable").innerHTML = createUserTable(tableUser);
 }
 
+//Hides or displays answer table - toggles
 function toggleAnswerTable() {
     var tbl = document.getElementById("answerTable");
     if (tbl.style.display === "none") {
@@ -145,6 +259,7 @@ function toggleAnswerTable() {
     }
 }
 
+//Compares user input to answer guide
 function submitAnswer() {
     var correct = false;
     for (var i = 0; i < tableUser.length; i++) {
@@ -162,10 +277,35 @@ function submitAnswer() {
     }
 
     if (correct) {
-        alert("CORRECT!!!!");
-    } else {
-        alert("WRONG >.<");
+        alert("CORRECT!!!! Congratulations!");
     }
+}
+
+//Switches to a different puzzle - toggles
+function changeNono() {
+    //if hard puzzle is displayed, switch to easier one
+    if (hardPuzzleChosen) {
+        for (var i = 0; i < tableAnswer.length; i++) {
+            for (var j = 0; j < tableAnswer[i].length; j++) {
+                tableAnswer[i][j] = tableAnswerEasy[i][j];
+                tableUser[i][j] = tableUserEasy[i][j];
+            }
+        }
+        hardPuzzleChosen = false;
+
+        //if easy puzzle is chosen, switch to harder one
+    } else {
+        for (var i = 0; i < tableAnswer.length; i++) {
+            for (var j = 0; j < tableAnswer[i].length; j++) {
+                tableAnswer[i][j] = tableAnswerHard[i][j];
+                tableUser[i][j] = tableUserHard[i][j];
+            }
+        }
+        hardPuzzleChosen = true;
+    }
+
+    document.getElementById("userTable").innerHTML = createUserTable(tableUser);
+    document.getElementById("answerTable").innerHTML = createTable(tableAnswer);
 }
 
 document.getElementById("userTable").innerHTML = createUserTable(tableUser);
